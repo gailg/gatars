2317f4cfef8253ca2497713dfd8d23d111ed07fc
Gail Gong <gailgongster@gmail.com>
2017-08-23 15:45:33 -0700

    The statistic name C changed to T. Also when C appeared as a component of the statistic.

--- R/short_names_fn.R
+++ R/short_names_fn.R
@@ -2,9 +2,9 @@
 short_names_fn = function(names){
   ifelse(names =="standard_burden", "B",
   ifelse(names =="standard_skat",   "S",
-  ifelse(names == "altern_skat",    "C",
+  ifelse(names == "altern_skat",    "T",
   ifelse(names == "standard_lee",   "BS",
-  ifelse(names == "optim_skat",     "SC",
-  ifelse(names == "odd",            "BC",
-                                    "BSC"))))))
+  ifelse(names == "optim_skat",     "ST",
+  ifelse(names == "odd",            "BT",
+                                    "BST"))))))
 }
