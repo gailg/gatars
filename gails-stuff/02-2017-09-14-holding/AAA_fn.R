e49c904bf928028ed98e5717a698b20ee0a4cc19
Gail Gong <gailgongster@gmail.com>
2017-09-18 06:59:38 -0700

    Cleaning up.  Next up work on the example files so I can record how I got them and also make sure I use either start or begin but not both.

--- gails-stuff/02-2017-09-14-holding/AAA_fn.R
+++
@@ -1,8 +0,0 @@
-#' @export
-AAA_fn = function(alpha, JJJ, III){
-  A_1 = alpha[1] * JJJ + alpha[2] * III
-  A_2 = alpha[3] * JJJ + alpha[4] * III
-  AAA =  rbind(cbind(A_1 + A_2, -A_1),
-               cbind(-A_1, A_1 - A_2))
-  AAA
-}
--- gails-stuff/02-2017-09-14-holding/alpha_lu_with_theta_3_zeroed_fn.R
+++
@@ -1,8 +0,0 @@
-#' @export
-alpha_lu_with_theta_3_zeroed_fn = function(theta){
-  a1 = cos(theta[1])^2
-  a2 = ( sin(theta[1]) * cos(theta[2]) )^2
-  a3 = 0
-  a4 = ( sin(theta[1]) * sin(theta[2]) )^2
-  c(a1 = a1, a2 = a2, a3 = a3, a4 = a4)
-}
