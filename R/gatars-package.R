8c965e1db59a668faa4b4a40e0e2e6c0876a6d87
Gail Gong <gailgongster@gmail.com>
2017-11-10 19:39:54 -0800

    gatars_test_size has been roxygenated

--- R/gatars-package.R
+++ R/gatars-package.R
@@ -2,9 +2,8 @@
 #'
 #' Test the association between a specified set of \code{MMM} target markers and a binary
 #' or quantitative trait using subjects with any genealogical relationship.
-#' v-0.2.15
 #' 
-#' \strong{The three functions used in \url{http://stanford.edu/~ggong/gatars/} are:}
+#' \strong{The three functions featured in \url{http://stanford.edu/~ggong/gatars/} are:}
 #' 
 #' \code{gatars_sampling_set}: Create the sampling sets that are used in genome resampling.
 #' 
@@ -13,14 +12,12 @@
 #'   
 #' \code{matrix_image_fn}: A little helper function to display the Psi matrix.
 #' 
-#' \strong{The datasets used are:}
+#' \strong{The datasets included are:}
 #' 
-#' \code{gatars_example}: A list of the 6 objects you need to provide to run \code{gatars}.
+#' \code{gatars_example}: A list of the 6 objects you need to provide to run \code{gatars}. Use this example to learn how to run \code{gatars}.
 #' 
 #' \code{hotspot}: The hotspot data used to create the independent segments that are used by \code{gatars_sampling_set}.
 #' 
-#' The \code{hotspot} data are from “A Fine-Scale Map of Recombination Rates and Hotspots Across the Human Genome”, Myers et.al., \strong{Science} 
-#' October 2005.
 #' 
 #' @docType package
 #' @name gatars
--- R/gatars_test_size.R
+++ R/gatars_test_size.R
@@ -1,23 +1,25 @@
-#' @title Test the association between genetic markers and disease. 
+#' @title Test the association between \code{MMM} prespecified genetic markers and a trait  
 #' 
-#' @description (Genetic Association Tests for Arbitrarily Related Subjects) 
-#' Calculate the p-values of a family of statistics that test the association between
-#' a specified set of \code{MMM} makrders and a binary trait.  These statistics include three 
+#' @description 
+#' Calculate the p-values of a family of statistics to test the association between
+#' a specified set of \code{MMM} markers and a binary or quantitative trait.  
+#' These statistics include three basic 
 #' statistics--the squared burden statistic \eqn{Q_B}, the linear kernel (SKAT) statistic
-#' \eqn{Q_S}, a new case-based kernel statistic \eqn{Q_C}--as well as an ensemble of four
-#' statistics \eqn{Q_BS}, \eqn{Q_BC}, \eqn{Q_SC}, \eqn{Q_BSC} that optimize linear combinations
+#' \eqn{Q_S}, and a new trait-based kernel statistic \eqn{Q_T}--as well as an ensemble of four
+#' statistics \eqn{Q_BS}, \eqn{Q_BT}, \eqn{Q_ST}, \eqn{Q_BST} that optimize linear combinations
 #' of the three basic statistics.  See \url{http://stanford.edu/~ggong/gatars/} for more details.
 #' 
 #' @param phenotype A data.frame containing \code{NNN} rows and at least the two columns
-#' \code{y} and \code{mu}, where \code{y[nnn]} is an indicator for the \code{nnn}-th person
-#' having disease and \code{mu[nnn]} is is a trait probability, the predicted value of 
-#' \code{y[nnn]} based on nongenetic covariates and possibly principal components of ancestry. 
-#' After we saw that principal conponents of ancestry were insignificant, we performed a 
-#' logistic regression of \code{y} on (1) age, (2) membership in family or case/control data, 
-#' and (3) their interaction, and used \code{mu} to be the fitted values of the logistic 
-#' regression.
+#' \code{y} and \code{mu}.  \code{y[nnn]} is the binary or quantitative trait of the 
+#' \code{nnn}-th person.  If the trait is binary, possible values are \code{0} for not affected
+#' and \code{1} for affected; if the trait is quantitative, possible values are real numbers.
+#' \code{mu[nnn]}, a real number, is  a trait prediction of 
+#' \code{y[nnn]}.  (For example, \code{mu[nnn]} can be the mean of the \code{NNN} \code{y[nnn]}
+#' values or the value predicted from a logistic or linear regression of \code{y[nnn]}
+#' on nongenetic covariates and/or on principal components of ancestry.)
 #' 
-#' @param Psi A matrix with \code{NNN} rows and \code{NNN} columns.  \code{Psi[n1, n2]} is the 
+#' @param Psi A real-valued matrix with \code{NNN} rows and \code{NNN} columns.  
+#' \code{Psi[n1, n2]} is the 
 #' correlation for the genotype at one marker between the \code{n1}-th and \code{n2}-th 
 #' subjects. \code{Psi} can be estimated from known family pedigree structures and/or from
 #' the subjects' genotypes at markers independent of those in the target set. The diagonal 
@@ -25,14 +27,16 @@
 #' have parents known to be completely unrelated have correlation 0.5.
 #' 
 #' @param sampling_set An object of class \code{gatars_sampling_set} produced by 
-#' \code{gatars_sampling_set}. (See \code{\link{gatars_sampling_set}}.)  The object \code{sampling_set} is list containing the objects
+#' \code{gatars_sampling_set}. (See \code{\link{gatars_sampling_set}}.)  
+#' The object \code{sampling_set} is list containing the objects
 #' \code{params_sampling_set}, \code{report}, and \code{sampling_set}.  The important object
-#' is \code{sampling_set$sampling_set}, a list containing \code{MMM} matrices. When it is time 
-#' for genome resampling, \code{gatars} creates "simulated genotype matrices" by sampling
-#' and cbinding together one column from each of the \code{MMM} matrices.  
+#' is \code{sampling_set$sampling_set}, a list containing \code{MMM} matrices. 
+#' To run genome resampling, \code{gatars_test_size}
+#' creates "simulated genotype matrices" by sampling
+#' and binding together one column from each of the \code{MMM} matrices.  
 #' 
-#' @param N_simulated_nulls An integer equal to the number of "simulated genotype matrices to generate
-#' when calculating the test sizes of the optimal statistics.
+#' @param N_simulated_nulls An integer equal to the number of simulated genotype matrices to generate
+#' when calculating the test sizes of the optimized statistics.
 
 
 #' @template gatars_test_size_examples
